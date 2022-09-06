0400909f75c6b5c434619f5320deeface82059a4
Gilian Rehm <st172032@stud.uni-stuttgart.de>
2022-09-07 01:10:36 +0200

    replace $i . 1 with $i + 1

--- setup.php
+++ setup.php
@@ -37,33 +37,33 @@ if ($numberOfOperations != 0 && empty($_POST)) {
             <p>-------------------------</p>
 
             <input name="numberOfOperations" value="<?php echo $numberOfOperations; ?>" hidden>
-            <input name="operationNumber<?php echo "-" . $i . 1; ?>" value="<?php echo $i + 1; ?>" hidden>
+            <input name="operationNumber<?php echo "-" . ($i + 1); ?>" value="<?php echo $i + 1; ?>" hidden>
             <label for="operationDescription">Einsatzbeschreibung</label>
-            <input type="text" id="operationDescription" name="operationDescription<?php echo "-" . $i . 1; ?>"><br>
+            <input type="text" id="operationDescription" name="operationDescription<?php echo "-" . ($i + 1); ?>"><br>
             <label for="address">Adresse</label>
-            <input type="text" id="address" name="address<?php echo "-" . $i . 1; ?>"><br>
+            <input type="text" id="address" name="address<?php echo "-" . ($i + 1); ?>"><br>
             <label for="operationDate">Einsatzdatum</label>
-            <input type="date" id="operationDate" name="operationDate<?php echo "-" . $i . 1; ?>"><br>
+            <input type="date" id="operationDate" name="operationDate<?php echo "-" . ($i + 1); ?>"><br>
             <label for="operationTime">Einsatzuhrzeit</label>
-            <input type="time" id="operationTime" name="operationTime<?php echo "-" . $i . 1; ?>"><br>
+            <input type="time" id="operationTime" name="operationTime<?php echo "-" . ($i + 1); ?>"><br>
 
             <label for="1/19">1/19</label>
-            <input type="checkbox" id="1/19" name="1/19<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="1/19" name="1/19<?php echo "-" . ($i + 1); ?>"><br>
             <label for="1/42">1/42</label>
-            <input type="checkbox" id="1/42" name="1/42<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="1/42" name="1/42<?php echo "-" . ($i + 1); ?>"><br>
             <label for="2/42">2/42</label>
-            <input type="checkbox" id="2/42" name="2/42<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="2/42" name="2/42<?php echo "-" . ($i + 1); ?>"><br>
             <label for="3/48">3/48</label>
-            <input type="checkbox" id="3/48" name="3/48<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="3/48" name="3/48<?php echo "-" . ($i + 1); ?>"><br>
 
             <label for="oil">Öl</label>
-            <input type="checkbox" id="oil" name="oil<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="oil" name="oil<?php echo "-" . ($i + 1); ?>"><br>
             <label for="hoseCart">SW</label>
-            <input type="checkbox" id="hoseCart" name="hoseCart<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="hoseCart" name="hoseCart<?php echo "-" . ($i + 1); ?>"><br>
             <label for="drk">DRK</label>
-            <input type="checkbox" id="drk" name="drk<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="drk" name="drk<?php echo "-" . ($i + 1); ?>"><br>
             <label for="police">POL</label>
-            <input type="checkbox" id="police" name="police<?php echo "-" . $i . 1; ?>"><br>
+            <input type="checkbox" id="police" name="police<?php echo "-" . ($i + 1); ?>"><br>
 
             <?php
         }
@@ -81,21 +81,21 @@ if (!empty($_POST)) {
 
     for ($i = 0; $i < $_POST['numberOfOperations']; $i++) {
         $postArray = array(
-            "operationNumber" => $_POST['operationNumber' . "-" . $i . 1],
-            "operationDescription" => $_POST['operationDescription' . "-" . $i . 1],
-            "address" => $_POST['address' . "-" . $i . 1],
-            "operationDate" => $_POST['operationDate' . "-" . $i . 1],
-            "operationTime" => $_POST['operationTime' . "-" . $i . 1],
+            "operationNumber" => $_POST['operationNumber' . ("-" . ($i + 1))],
+            "operationDescription" => $_POST['operationDescription' . ("-" . ($i + 1))],
+            "address" => $_POST['address' . ("-" . ($i + 1))],
+            "operationDate" => $_POST['operationDate' . ("-" . ($i + 1))],
+            "operationTime" => $_POST['operationTime' . ("-" . ($i + 1))],
 
-            "1-19" => $_POST['1/19' . "-" . $i . 1] ?? "off",
-            "1-42" => $_POST['1/42' . "-" . $i . 1] ?? "off",
-            "2-42" => $_POST['2/42' . "-" . $i . 1] ?? "off",
-            "3-48" => $_POST['3/48' . "-" . $i . 1] ?? "off",
+            "1-19" => $_POST['1/19' . ("-" . ($i + 1))] ?? "off",
+            "1-42" => $_POST['1/42' . ("-" . ($i + 1))] ?? "off",
+            "2-42" => $_POST['2/42' . ("-" . ($i + 1))] ?? "off",
+            "3-48" => $_POST['3/48' . ("-" . ($i + 1))] ?? "off",
 
-            "oil" => $_POST['oil' . "-" . $i . 1] ?? "off",
-            "hoseCart" => $_POST['hoseCart' . "-" . $i . 1] ?? "off",
-            "drk" => $_POST['drk' . "-" . $i . 1] ?? "off",
-            "police" => $_POST['police' . "-" . $i . 1] ?? "off"
+            "oil" => $_POST['oil' . ("-" . ($i + 1))] ?? "off",
+            "hoseCart" => $_POST['hoseCart' . ("-" . ($i + 1))] ?? "off",
+            "drk" => $_POST['drk' . ("-" . ($i + 1))] ?? "off",
+            "police" => $_POST['police' . ("-" . ($i + 1))] ?? "off"
         );
 
         $json .= json_encode($postArray);
